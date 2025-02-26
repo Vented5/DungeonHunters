c6e17a9e9a3c8700a81907f0b38b4e85cdc93af0
Vented5 <venteddiagram@gmail.com>
2025-02-26 15:22:14 -0600

    scene(player/sword/camera): Sincronized collisions with attack inputs, added attack touchscreenbutton

--- main.gd
+++ main.gd
@@ -2,20 +2,21 @@ extends Node
 
 @export var slime_scene: PackedScene
 @export var player_scene: PackedScene
+var mob_spawn
 
 func _ready():
 	#var player = player_scene.instantiate()
 	#player.position = Vector2(640, 360)
 	#add_child(player)
 	
-	var mob_spawn = $Mob_spawn_path/Mob_spawn_location
-	
-	for i in 2:
-		var slime = slime_scene.instantiate()
-		mob_spawn.progress_ratio = randf()
-		slime.position = mob_spawn.position
-		add_child(slime)
-
+	mob_spawn = $Mob_spawn_path/Mob_spawn_location
+	$Slime_timer.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_slime_timer_timeout():
+	var slime = slime_scene.instantiate()
+	mob_spawn.progress_ratio = randf()
+	slime.position = mob_spawn.position
+	add_child(slime)
--- player.gd
+++ player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@onready var joystick = $"Camera2D/Joystick"
+@onready var joystick
 @export var weapon_scene: PackedScene
 var weapon
 var weapon_animation
@@ -7,7 +7,7 @@ var speed = 300
 var direction
 
 var radius: float = 150
-var attack_duration: float = 10
+var attack_duration: float = 0.2
 var initial_angle
 var final_angle
 var attack_tween
@@ -15,6 +15,9 @@ var attack_tween
 func _ready():
 	#weapon = weapon_scene.instantiate()
 	weapon = $Sword
+	joystick = $Camera2D/Joystick
+	weapon.collision_mask &= ~2
+	print("Máscara inicial:", weapon.collision_mask)
 	#weapon.collision_layer = 0
 	#add_child(weapon)
 
@@ -55,15 +58,22 @@ func _process(delta: float):
 
 	# -------------------------- Actions
 	if Input.is_action_just_pressed("jump"):
-		attack_tween = create_tween()
-		attack_tween.tween_method(attack, 0.0, 1.0, 0.2)
-		await attack_tween.finished
-		weapon.position = Vector2.ZERO
-		if direction.angle() >= (PI/2):
-			weapon.rotation = deg_to_rad(0)
-		else:
-			weapon.rotation = deg_to_rad(250)
-	
+		handle_attack()
+		
+func handle_attack ():
+	weapon.collision_mask |= 2 # Reactiva la mascara de colosion en la posision binaria 2
+	print("Máscara modificada:", weapon.collision_mask)
+	attack_tween = create_tween()
+	attack_tween.tween_method(attack, 0.0, 1.0, attack_duration)
+	await attack_tween.finished
+	weapon.collision_mask &= ~2
+	print("Máscara modificada:", weapon.collision_mask)
+		
+	weapon.position = Vector2.ZERO
+	if direction.angle() >= (PI/2):
+		weapon.rotation = deg_to_rad(0)
+	else:
+		weapon.rotation = deg_to_rad(250)
 
 func attack(t):
 	initial_angle = direction.angle() - PI/4
@@ -82,3 +92,7 @@ func attack(t):
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_touch_screen_button_pressed() -> void:
+	handle_attack()
